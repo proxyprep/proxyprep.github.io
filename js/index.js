7893aa40bf10750ed0a189c29a20c98f5c341400
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2020-09-27 13:47:56 +0530

    Added animation to nav-link

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                    scrollTop: Math.min(Math.max((target.offset().top) - 100, 0), $(document).height() - $(window).height())
+                    scrollTop: Math.min(Math.max((target.offset().top) - 50, 0), $(document).height() - $(window).height())
                 }, 1000, "easeInOutExpo");
                 return false;
             }
